fbe178aabc556a773fa4ddd969f9cf45563ef39f
lorekhov <lorekhov@yandex-team.ru>
2015-06-25 00:43:28 +0500

    password reset was added

--- taskshot/urls.py
+++ taskshot/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-import registration.backends.default.urls
-
 
 urlpatterns = [
     url(r'^', include('tasks.urls')),
